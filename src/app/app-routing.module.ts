d36c729e56ea0231030b38efdde6b320d595103b
Brian Ngilazi <bkcngilazi@gmail.com>
2022-06-24 16:51:03 +0200

    feature(page not found): Add 404 page and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, title: 'Home' },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', pathMatch: 'full', component: PageNotFoundComponent, title: 'Page Not Found'}
 ];
 
 @NgModule({
